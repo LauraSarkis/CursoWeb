ba7c9e52d966b7c0a6cbb6287cbd5c85c6ee35ae
LauraSarkis <laura.sarkis@ufac.br>
2022-11-30 15:15:56 +0000

    versão final

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 
+DATABASE = "banco.bd"
+SECRET_KEY = "1234"
 
 app = Flask("Olá mundo")
 
-@app.route("/alunos")
+@app.route("/")
 def alunos():
     return render_template("hello.html")
